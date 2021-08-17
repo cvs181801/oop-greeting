a5b17d2430a4a7e52fb271ce4dee897074d99c50
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-08-17 16:52:19 -0700

    grab input elemets from DOM

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 //grab elements from the DOM
 const submitBtn = document.querySelector(".container__form--submitBtn");
+const name = document.getElementById("name");
+const emoji = document.getElementById("fav-emoji");
+const favFood = document.getElementById("fav-food");
+const favDess = document.getElementById("fav-dessert");
+const favBev = document.getElementById("fav-bev");
+const favGenre = document.getElementById("fav-genre");
+const favCity = document.getElementById("fav-city");
+const favTime = document.getElementById("fav-time");
+const country = document.getElementById("country");
+
 //test area
+console.log(name);
+
 
 //add a way to add a new person into the group. lets go with ES5 syntax this time.
 
@@ -22,7 +34,7 @@ function Person(name, favoriteEmoji, favoriteFood, favoriteDessert, favoriteBeve
 
 
 submitBtn.addEventListener('submit', function(e) {
-//instantiate a new person
+    let person1 = new Person()
 })
 //add a way to select a new experience
 
