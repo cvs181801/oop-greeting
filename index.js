db951c4f398d1e114796ab354d5fd7e1a83768f9
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-08-22 11:17:02 -0700

    remove the emoji and fav time of year inputs from forms, add radio buttons for interests

--- index.js
+++ index.js
@@ -35,68 +35,56 @@ const formsList = document.getElementsByTagName(".form");
 
 //1st form (user) inputs
 const friendName = document.getElementById("name");
-const emoji = document.getElementById("fav-emoji");
 const favFood = document.getElementById("fav-food");
 const favDess = document.getElementById("fav-dessert");
 const favBev = document.getElementById("fav-bev");
 const favGenre = document.getElementById("fav-genre");
 const favCity = document.getElementById("fav-city");
-const favTime = document.getElementById("fav-time");
 const country = document.getElementById("country");
 
 //2nd friend form inputs
 const friendName2 = document.getElementById("name2");
-const emoji2 = document.getElementById("fav-emoji2");
 const favFood2 = document.getElementById("fav-food2");
 const favDess2 = document.getElementById("fav-dessert2");
 const favBev2 = document.getElementById("fav-bev2");
 const favGenre2 = document.getElementById("fav-genre2");
 const favCity2 = document.getElementById("fav-city2");
-const favTime2 = document.getElementById("fav-time2");
 const country2 = document.getElementById("country2");
 
 //3rd friend form
 const friendName3 = document.getElementById("name3");
-const emoji3 = document.getElementById("fav-emoji3");
 const favFood3 = document.getElementById("fav-food3");
 const favDess3 = document.getElementById("fav-dessert3");
 const favBev3 = document.getElementById("fav-bev3");
 const favGenre3 = document.getElementById("fav-genre3");
 const favCity3 = document.getElementById("fav-city3");
-const favTime3 = document.getElementById("fav-time3");
 const country3 = document.getElementById("country3");
 
 //4th friend form
 const friendName4 = document.getElementById("name4");
-const emoji4 = document.getElementById("fav-emoji4");
 const favFood4 = document.getElementById("fav-food4");
 const favDess4 = document.getElementById("fav-dessert4");
 const favBev4 = document.getElementById("fav-bev4");
 const favGenre4 = document.getElementById("fav-genre4");
 const favCity4 = document.getElementById("fav-city4");
-const favTime4 = document.getElementById("fav-time4");
 const country4 = document.getElementById("country4");
 
 //5th friend form
 const friendName5 = document.getElementById("name5");
-const emoji5 = document.getElementById("fav-emoji5");
 const favFood5 = document.getElementById("fav-food5");
 const favDess5 = document.getElementById("fav-dessert5");
 const favBev5 = document.getElementById("fav-bev5");
 const favGenre5 = document.getElementById("fav-genre5");
 const favCity5 = document.getElementById("fav-city5");
-const favTime5 = document.getElementById("fav-time5");
 const country5 = document.getElementById("country5");
 
 //6th friend form
 const friendName6 = document.getElementById("name6");
-const emoji6 = document.getElementById("fav-emoji6");
 const favFood6 = document.getElementById("fav-food6");
 const favDess6 = document.getElementById("fav-dessert6");
 const favBev6 = document.getElementById("fav-bev6");
 const favGenre6 = document.getElementById("fav-genre6");
 const favCity6 = document.getElementById("fav-city6");
-const favTime6 = document.getElementById("fav-time6");
 const country6 = document.getElementById("country6");
 
 //inner cards for the friends and user 
@@ -137,16 +125,14 @@ const formCloseBtns = document.querySelectorAll(".container__form--closeBtn");
 
 //add a way to add a new person into the group. lets go with ES5 syntax this time.
 
-function Person(name, favoriteEmoji, favoriteFood, favoriteDessert, favoriteBeverage, favoriteFilmGenre, favoriteCity, favoriteTimeOfYear,
+function Person(name, favoriteFood, favoriteDessert, favoriteBeverage, favoriteFilmGenre, favoriteCity,
     countryYoudLikeToVisit) {
     this.name = name;
-    this.favoriteEmoji = favoriteEmoji;
     this.favoriteFood = favoriteFood;
     this.favoriteDessert = favoriteDessert;
     this.favoriteBeverage = favoriteBeverage;
     this.favoriteFilmGenre = favoriteFilmGenre;
     this.favoriteCity = favoriteCity;
-    this.favoriteTimeOfYear = favoriteTimeOfYear;
     this.countryYoudLikeToVisit = countryYoudLikeToVisit;
     this.greeting = function() {
         return `Hi! I'm ${this.name}.`
@@ -179,10 +165,11 @@ button1.addEventListener("click", function(e) {
             friendForm.addEventListener("submit", function(e) {
             e.preventDefault();
             console.log(person1.name, person1.favoriteFood);
-        person1 = new Person(friendName.value, emoji.value, favFood.value, favDess.value, 
-            favBev.value, favGenre.value, favCity.value, favTime.value, country.value)
+        person1 = new Person(friendName.value, favFood.value, favDess.value, 
+            favBev.value, favGenre.value, favCity.value, country.value)
         person1Name.textContent = person1.name;
-        person1emoji.textContent = person1.favoriteEmoji;
+        person1emoji.textContent = "❧";
+        person1emoji.style.fontSize = "1.3rem";
         friendForm.classList.add("hidden");
         containerOne.classList.add("hiddenModal");
         personOne.setAttribute("title", `${person1.greeting()}`);
@@ -197,12 +184,13 @@ button1.addEventListener("click", function(e) {
 friendForm.addEventListener("submit", function(e) {
     e.preventDefault();
     
-    person1 = new Person(friendName.value, emoji.value, favFood.value, favDess.value, 
-        favBev.value, favGenre.value, favCity.value, favTime.value, country.value)
+    person1 = new Person(friendName.value, favFood.value, favDess.value, 
+        favBev.value, favGenre.value, favCity.value, country.value)
        
     person1Name.textContent = person1.name;
     personOne.append(person1Name);
-    person1emoji.textContent = person1.favoriteEmoji;
+    person1emoji.textContent = "❧"
+    person1emoji.style.fontSize = "1.3rem";
     personOne.append(person1emoji);
     friendForm.classList.add("hidden");
     containerOne.classList.add("hiddenModal");
@@ -231,10 +219,11 @@ button2.addEventListener("click", function(e) {
         friendForm2.addEventListener("submit", function(e) {
             e.preventDefault();
             console.log(" person 2 edit object e listener worked");
-                person2 = new Person(friendName2.value, emoji2.value, favFood2.value, favDess2.value, 
-                favBev2.value, favGenre2.value, favCity2.value, favTime2.value, country2.value)
+                person2 = new Person(friendName2.value, favFood2.value, favDess2.value, 
+                favBev2.value, favGenre2.value, favCity2.value, country2.value)
         person2Name.textContent = person2.name;
-        person2emoji.textContent = person2.favoriteEmoji;
+        person2emoji.textContent = "❡";
+        person2emoji.style.fontSize = "1.3rem";
         welcomeGreet.textContent = person2.welcome();
         friendForm2.classList.add("hidden");
         containerTwo.classList.add("hiddenModal");
@@ -248,12 +237,13 @@ button2.addEventListener("click", function(e) {
         containerTwo.classList.remove("hiddenModal");
             friendForm2.addEventListener("submit", function(e) {
                 e.preventDefault();
-                person2 = new Person(friendName2.value, emoji2.value, favFood2.value, favDess2.value, 
-                        favBev2.value, favGenre2.value, favCity2.value, favTime2.value, country2.value)
+                person2 = new Person(friendName2.value, favFood2.value, favDess2.value, 
+                        favBev2.value, favGenre2.value, favCity2.value, country2.value)
                 console.log(" person 2 instantiation e listener worked");
                 person2Name.textContent = person2.name;
                 personTwo.append(person2Name);
-                person2emoji.textContent = person2.favoriteEmoji;
+                person2emoji.textContent = "❡";
+                person2emoji.style.fontSize = "1.3rem";
                 personTwo.append(person2emoji);
                 welcomeGreet.textContent = person2.welcome();
                 friendForm2.classList.add("hidden");
@@ -281,12 +271,13 @@ button3.addEventListener("click", function(e) {
         containerThree.classList.remove("hiddenModal");
             friendForm3.addEventListener("submit", function(e) {
                 e.preventDefault();
-                person3 = new Person(friendName3.value, emoji3.value, favFood3.value, favDess3.value, 
-                        favBev3.value, favGenre3.value, favCity3.value, favTime3.value, country3.value)
+                person3 = new Person(friendName3.value, favFood3.value, favDess3.value, 
+                        favBev3.value, favGenre3.value, favCity3.value, country3.value)
                 
                 console.log(" person 3 edit e listener worked");
                 person3Name.textContent = person3.name;
-                person3emoji.textContent = person3.favoriteEmoji;
+                person3emoji.textContent = "✷";
+                person3emoji.style.fontSize = "1.3rem";
                 welcomeGreet.textContent = person3.welcome();
                 friendForm3.classList.add("hidden");
                 containerThree.classList.add("hiddenModal");
@@ -300,12 +291,13 @@ button3.addEventListener("click", function(e) {
                 containerThree.classList.remove("hiddenModal");
                     friendForm3.addEventListener("submit", function(e) {
                         e.preventDefault();
-                        person3 = new Person(friendName3.value, emoji3.value, favFood3.value, favDess3.value, 
-                        favBev3.value, favGenre3.value, favCity3.value, favTime3.value, country3.value)
+                        person3 = new Person(friendName3.value, favFood3.value, favDess3.value, 
+                        favBev3.value, favGenre3.value, favCity3.value, country3.value)
                         console.log(" person 3 instantiation e listener worked");
                         person3Name.textContent = person3.name;
                         personThree.append(person3Name);
-                        person3emoji.textContent = person3.favoriteEmoji;
+                        person3emoji.textContent = "✷";
+                        person3emoji.style.fontSize = "1.3rem";
                         personThree.append(person3emoji);
                         welcomeGreet.textContent = person3.welcome();
                         friendForm3.classList.add("hidden");
@@ -331,11 +323,12 @@ button4.addEventListener("click", function(e) {
             friendForm4.classList.remove("hidden");
                 friendForm4.addEventListener("submit", function(e) {
                     e.preventDefault();
-                    person4 = new Person(friendName4.value, emoji4.value, favFood4.value, favDess4.value, 
-                            favBev4.value, favGenre4.value, favCity4.value, favTime4.value, country4.value)
+                    person4 = new Person(friendName4.value, favFood4.value, favDess4.value, 
+                            favBev4.value, favGenre4.value, favCity4.value, country4.value)
                     console.log(" person 4 edit e listener worked");
                     person4Name.textContent = person4.name;
-                    person4emoji.textContent = person4.favoriteEmoji;
+                    person4emoji.textContent = "☙";
+                    person4emoji.style.fontSize = "1.3rem";
                     welcomeGreet.textContent = person4.welcome();
                     friendForm4.classList.add("hidden");
                     containerFour.classList.add("hiddenModal");
@@ -347,12 +340,13 @@ button4.addEventListener("click", function(e) {
                 friendForm4.classList.remove("hidden");
                     friendForm4.addEventListener("submit", function(e) {
                         e.preventDefault();
-                        person4 = new Person(friendName4.value, emoji4.value, favFood4.value, favDess4.value, 
-                                favBev4.value, favGenre4.value, favCity4.value, favTime4.value, country4.value)
+                        person4 = new Person(friendName4.value, favFood4.value, favDess4.value, 
+                                favBev4.value, favGenre4.value, favCity4.value, country4.value)
                         console.log(" person 4 instantiation e listener worked");
                         person4Name.textContent = person4.name;
                         personFour.append(person4Name);
-                        person4emoji.textContent = person4.favoriteEmoji;
+                        person4emoji.textContent = "☙";
+                        person4emoji.style.fontSize = "1.3rem";
                         personFour.append(person4emoji);
                         welcomeGreet.textContent = person4.welcome();
                         friendForm4.classList.add("hidden");
@@ -376,11 +370,12 @@ button5.addEventListener("click", function(e) {
         containerFive.classList.remove("hiddenModal");
             friendForm5.addEventListener("submit", function(e) {
                 e.preventDefault();
-                person5 = new Person(friendName5.value, emoji5.value, favFood5.value, favDess5.value, 
-                        favBev5.value, favGenre5.value, favCity5.value, favTime5.value, country5.value)
+                person5 = new Person(friendName5.value, favFood5.value, favDess5.value, 
+                        favBev5.value, favGenre5.value, favCity5.value, country5.value)
                 console.log(" person 5 edit e listener worked");
                 person5Name.textContent = person5.name;
-                person5emoji.textContent = person5.favoriteEmoji;
+                person5emoji.textContent = "❦";
+                person5emoji.style.fontSize = "1.3rem";
                 welcomeGreet.textContent = person5.welcome();
                 friendForm5.classList.add("hidden");
                 containerFive.classList.add("hiddenModal");
@@ -392,12 +387,13 @@ button5.addEventListener("click", function(e) {
         containerFive.classList.remove("hiddenModal");
             friendForm5.addEventListener("submit", function(e) {
                 e.preventDefault();
-                person5 = new Person(friendName5.value, emoji5.value, favFood5.value, favDess5.value, 
-                        favBev5.value, favGenre5.value, favCity5.value, favTime5.value, country5.value)                
+                person5 = new Person(friendName5.value, favFood5.value, favDess5.value, 
+                        favBev5.value, favGenre5.value, favCity5.value, country5.value)                
                 console.log(" person 5 instantiation e listener worked");            
                 person5Name.textContent = person5.name;
                 personFive.append(person5Name);
-                person5emoji.textContent = person5.favoriteEmoji;
+                person5emoji.textContent = "❦";
+                person5emoji.style.fontSize = "1.3rem";
                 personFive.append(person5emoji);               
                 welcomeGreet.textContent = person5.welcome();
                 friendForm5.classList.add("hidden");
@@ -421,11 +417,12 @@ button6.addEventListener("click", function(e) {
         friendForm6.classList.remove("hidden");
             friendForm6.addEventListener("submit", function(e) {
                 e.preventDefault();
-                person6 = new Person(friendName6.value, emoji6.value, favFood6.value, favDess6.value, 
-                        favBev6.value, favGenre6.value, favCity6.value, favTime6.value, country6.value)
+                person6 = new Person(friendName6.value, favFood6.value, favDess6.value, 
+                        favBev6.value, favGenre6.value, favCity6.value, country6.value)
                 console.log(" person 6 edit e listener worked");
                 person6Name.textContent = person6.name;
-                person6emoji.textContent = person6.favoriteEmoji;
+                person6emoji.textContent = "✰";
+                person6emoji.style.fontSize = "1.3rem";
                 welcomeGreet.textContent = person6.welcome();
                 friendForm6.classList.add("hidden");
                 containerSix.classList.add("hiddenModal");
@@ -437,12 +434,13 @@ button6.addEventListener("click", function(e) {
         friendForm6.classList.remove("hidden");
         friendForm6.addEventListener("submit", function(e) {
             e.preventDefault();
-            person6 = new Person(friendName6.value, emoji6.value, favFood6.value, favDess6.value, 
-                favBev6.value, favGenre6.value, favCity6.value, favTime6.value, country6.value)
+            person6 = new Person(friendName6.value, favFood6.value, favDess6.value, 
+                favBev6.value, favGenre6.value, favCity6.value, country6.value)
             console.log(" person 6 instantiation e listener worked");
             person6Name.textContent = person6.name;
             personSix.append(person6Name);
-            person6emoji.textContent = person6.favoriteEmoji;
+            person6emoji.textContent = "✰";
+            person6emoji.style.fontSize = "1.3rem";
             personSix.append(person6emoji);
             welcomeGreet.textContent = person6.welcome();
             friendForm6.classList.add("hidden");
@@ -454,7 +452,7 @@ button6.addEventListener("click", function(e) {
 
 //**~~  Create a way to compare each person's data/favs and suggest experience ideas!  ~~**//
 
-//1.Fav foods person 1 and 2:
+//Fav foods person 1 and 2:
 const suggestFood = document.createElement("div");
 suggestFood.classList.add("personcards__suggest");
 //suggestFood.style.gridColumn = "4 / 5";
@@ -472,7 +470,7 @@ function checkFoods1and2() {
         }
     }
 }
-//2. Fav desserts person 1 and 2:
+// Fav desserts person 1 and 2:
 const suggestDessert = document.createElement("div");
 suggestDessert.classList.add("personcards__suggest");
 cardContainer.insertBefore(suggestDessert, containerCardTwo);
@@ -491,7 +489,7 @@ function checkDessert() {
     }
 }
 
-//3. Fav food 1, 2, and 3 
+//Fav food 1, 2, and 3 
 const suggestFood3 = document.createElement("div");
 suggestFood3.classList.add("personcards__suggest");
 suggestFood3.style.gridColumn = "1";
@@ -516,7 +514,7 @@ function checkFoods12and3() {
     }
 }
 
-//4. Fav Dessert 1, 2 and 3 
+//Fav Dessert 1, 2 and 3 
 
 const suggestDessert3 = document.createElement("div");
 suggestDessert3.classList.add("personcards__suggest");
